af3eb56fc915cae11a38219513690e811a4f79da
kasur.rusak404@gmail.com <1631Nz94@>
2023-07-20 15:05:49 +0700

    start awal

---
+++ bin/muji_belajar_dart.dart
@@ -0,0 +1,5 @@
+import 'package:muji_belajar_dart/muji_belajar_dart.dart' as muji_belajar_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${muji_belajar_dart.calculate()}!');
+}
---
+++ test/muji_belajar_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:muji_belajar_dart/muji_belajar_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
