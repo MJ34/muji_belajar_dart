ce32da0558ee89b7adf32c3e9dcd0ec4a3858a41
kasur.rusak404@gmail.com <1631Nz94@>
2023-08-11 10:34:22 +0700

    belajar type data number dart

---
+++ bin/number.dart
@@ -0,0 +1,15 @@
+/// *NUMBER*
+/* 
+* Number adalah tipe data angka, terdapat dua jenis tipe data Number, int dan double
+* int adalah tipe data bilangan bulat
+* double adalah tipe data bilangan desimal
+* Penulisan koma dalam double menggunakan titik, bukan koma, jadi jika kita akan membuat bilangan desimal 0,5 (nol koma lima), maka ditulis 0.5
+* Jika kita ingin menggunakan tipe data number yang bisa int ataupun double, kita bisa menggunakan tipe data num
+ */
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+}
