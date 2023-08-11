d49480391d3ecf6c369605f897421c550988e7d1
kasur.rusak404@gmail.com <1631Nz94@>
2023-08-11 11:32:37 +0700

    Operator Perbandingan dart

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,19 @@
+// *********** Operator Perbandingan ****************
+/* 
+1. Operator perbandingan adalah operator yang hasilnya adalah bool
+2. Operator perbandingan bisa digunakan di tipe data number dan string
+ */
+
+void main(){
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('muji' == 'muji');
+  print('muji' != 'muji');
+
+}
