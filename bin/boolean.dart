ad75c469695cd4dd881241aeb38b5eac52accffa
kasur.rusak404@gmail.com <1631Nz94@>
2023-08-11 10:40:16 +0700

    belajar boolean dart

---
+++ bin/boolean.dart
@@ -0,0 +1,14 @@
+// * BOOLEAAN*
+/* 
+1. Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu benar atau salah
+2. Boolean direpresentasikan dengan kata kunci bool
+3. Untuk nilai benar, menggunakan kata kunci true
+4. Untuk nilai salah, menggunakan kata kunci false */
+
+void main() {
+  bool finish = false;
+  print(finish);
+
+  finish = true;
+  print(finish);
+}
