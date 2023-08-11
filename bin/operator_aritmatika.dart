21062744cb3ca7ab3971aa30820fbb65e44972d1
kasur.rusak404@gmail.com <1631Nz94@>
2023-08-11 11:29:03 +0700

    Operator Aritmatika dart

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,23 @@
+// ********** Operator Aritmatika ***************
+/* 
+1. + Pertambahan
+2. - , -expression Pengurangan atau Negatif
+3. * Perkalian
+4. / Pembagian, Hasil double
+5. ~/ Pembagian, Hasil int
+6. % Sisa Bagi
+ */
+
+void main(){
+
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+
+}
